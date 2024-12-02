4eabbf38febd1ad757715515a1f525f1665d2e91
Sean Moss <smoss117@gmail.com>
2024-12-01 22:45:29 -0500

    Fixed some syntax issues

--- Day_1/day_1.py
+++ Day_1/day_1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def load_data(filepath="input", delimiter="   "):
+def load_data(filepath="input", delimiter="   ") -> tuple[pd.Series, pd.Series]:
     try:
         data = pd.read_csv(filepath, header=None, sep=delimiter, 
                           names=['val_1', 'val_2'])
@@ -12,8 +12,9 @@ def load_data(filepath="input", delimiter="   "):
     except Exception as e:
         raise ValueError(f"Error processing input data: {str(e)}")
 
-def part_1():
-    return  np.sum(np.absolute(np_array_1 - np_array_2))
+
+def part_1(array1: pd.Series, array2: pd.Series) -> float:
+    return np.sum(np.absolute(array1 - array2)) # type: ignore
 
 def part_2(array1: pd.Series, array2: pd.Series) -> float:
     """Calculate weighted sum where each value is multiplied by its occurrence count.
@@ -26,7 +27,8 @@ def part_2(array1: pd.Series, array2: pd.Series) -> float:
         Weighted sum based on occurrence counts
     """
     value_counts = array2.value_counts()
-    return np.sum(array1 * array1.map(value_counts.get).fillna(0))
+    return np.sum(array1 * array1.map(value_counts.get).fillna(0)) # type: ignore
 
-print(part_1())
-print(part_2())
+input_data = load_data()
+print(part_1(*input_data))
+print(part_2(*input_data))
